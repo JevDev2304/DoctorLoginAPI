1a1b4bcfac88fdc8dca5f9eba16dbe45e10b904c
Hidalgo87 <juanfgomez6@gmail.com>
2025-07-01 15:39:09 -0500

    Permitiendo todos los origenes para el consumo de la API

--- app/main.py
+++ app/main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
 from app.routes import doctor_routes
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(doctor_routes.router, prefix="/doctor")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins="*",
+    allow_credentials=True,
+    allow_methods="*",
+    allow_headers="*"
+)
 
 @app.get("/")
 def root():
