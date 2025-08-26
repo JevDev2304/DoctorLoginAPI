1d399dc3b808fa9a51d3c14f227fb2881851b87a
JevDev2304 <110129722+JevDev2304@users.noreply.github.com>
2025-08-25 20:56:41 -0500

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,7 +2,11 @@ from fastapi import FastAPI
 from app.routes import admin_routes, doctor_routes
 from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+app = FastAPI(
+    root_path="/hine/doctorlogin",
+    title="DoctorLogin API",
+
+)
 
 app.include_router(doctor_routes.router, prefix="/doctor")
 app.include_router(admin_routes.router, prefix="/admin")
